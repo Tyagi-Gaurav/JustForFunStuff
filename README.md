78e4261ded9098374ed5fe7f2c6b39036e27260d
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-04-16 19:59:26 +0100

    Some refactoring

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # JustForFunStuff
 
-Problems:
-
-* If the Ad does not load then page is not loaded
-* Page load takes long time (Could be due to the Ad)
-* Can Ad be part of a React component so it can be disabled in test
--- tests/tic-tac-toe.spec.js
+++ tests/tic-tac-toe.spec.js
@@ -3,19 +3,19 @@ import { test, expect } from "@playwright/test";
 test.describe("Main Page", () => {
   test.describe("Layout", () => {
     test("has status that shows who is the next player", async ({ page }) => {
-      await page.goto("http://localhost:3000");
+      await gotoMainPage(page);
       const status = page.getByText("Next Player: X");
       await expect(status).toBeVisible();
     });
 
     test("has a restart button", async ({ page }) => {
-      await page.goto("http://localhost:3000");
+        await gotoMainPage(page);
       const restartButton = page.getByRole("button", { name: "Restart" });
       await expect(restartButton).toBeVisible();
     });
 
     test("has a grid of 9 squares", async ({ page }) => {
-      await page.goto("http://localhost:3000");
+        await gotoMainPage(page);
 
       for (let index = 1; index <= 9; index++) {
         const square = page.locator("#square-"+index);
@@ -24,3 +24,7 @@ test.describe("Main Page", () => {
     });
   });
 });
+async function gotoMainPage(page) {
+    await page.goto("http://localhost:3000");
+}
+
