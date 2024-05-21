03c58ef2102109b3a40f255a5ebb905311e252cd
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-05-21 11:18:55 +0100

    More layout changes for Vocabulary

--- src/pages/VocabularyTesting.js
+++ src/pages/VocabularyTesting.js
@@ -7,35 +7,53 @@ export default function VocabularyTesting() {
         <h1>Test your Vocabulary</h1>
       </div>
       <div className="row mb-2">
-        <div className="col-sm-12">
-          <h3 className="border display-1 rounded bg-light">Word</h3>
+        <div className="col-sm-10">
+          <h3 className="border display-1 rounded bg-light">Enact</h3>
         </div>
       </div>
 
       <div className="row mb-2">
-        <label>Can you think of the answer before the timer runs out?</label>
+        <label>Can you think of an answer before the timer runs out?</label>
         <p></p>
       </div>
 
-      <div className="row mb-2">
-        <div className="col-sm-12">
-          <h4 className="border display-3 rounded bg-light">Meaning</h4>
+      <div className="row p-0">
+        <div className="col-sm-2">
+          <label>Meaning</label>
+        </div>
+        <div className="col-sm-9 ml-1">
+          <h5 className="border display-5 rounded bg-light">
+          Make Law<br/>
+          put into practice (an idea or suggestion)<br/>
+          act out (a role or play) on stage<br/>
+          </h5>
         </div>
       </div>
 
-      <div className="row mb-2">
-        <div className="col-sm-12">
-          <h4 className="border display-3 rounded bg-light">Synonyms</h4>
+      <div className="row p-0">
+        <div className="col-sm-2">
+          <label>Synonyms</label>
+        </div>
+        <div className="col-sm-9 ml-1">
+          <h5 className="border display-5 rounded bg-light">
+          Pass<br/>
+          Approve<br/>
+          Perform<br/>
+          Play<br/>
+          </h5>
         </div>
       </div>
 
-      <div className="row mb-2">
-        <div className="col-sm-12">
-          <h4 className="border display-3 rounded bg-light">Antonyms</h4>
+      <div className="row p-0">
+      <div className="col-sm-2">
+          <label>Example</label>
+        </div>
+        <div className="col-sm-9 ml-1">
+          <h5 className="border display-5 rounded bg-light">Example Usage</h5>
         </div>
       </div>
 
-      <div className="row mb-2 mt-10">
+      <div className="row">
         <div className="col-sm-12 replay justify-content-center pb-10">
           <button className="btn btn-primary">Next</button>
         </div>
