bc677a040e59db57a643645136e1f668cfb32103
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-06-02 16:15:23 +0100

    Generating application jar using spring maven plugin and enabling actuator

--- jffs-acceptance-tests/src/test/kotlin/com/jffs/tests/VocabResourceTest.kt
+++ jffs-acceptance-tests/src/test/kotlin/com/jffs/tests/VocabResourceTest.kt
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.ApplicationContext
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
@@ -24,9 +23,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(classes = [JffsApplication::class])
 class VocabResourceTest {
-    @Autowired
-    val applicationContext: ApplicationContext? = null
-
     @Autowired
     val mongoClient: MongoClient? = null;
     var collection: MongoCollection<Word>? = null
