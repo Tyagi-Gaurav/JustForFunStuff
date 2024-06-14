fb6ac355901bdfa80c49de52965dfab35ffd3073
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-06-14 14:51:54 +0100

    Admin endpoint with pagination

--- jffs-admin-backend/src/main/kotlin/com/jffs/admin/app/resource/AdminController.kt
+++ jffs-admin-backend/src/main/kotlin/com/jffs/admin/app/resource/AdminController.kt
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
-class VocabController(@Autowired val vocabRepository: VocabRepository) {
-    @GetMapping("/v1/words", produces = ["application/json"])
-    suspend fun getWords(): ResponseEntity<WordsDTO> {
+class AdminController(@Autowired val vocabRepository: VocabRepository) {
+    @GetMapping("/v1/words/{pageNum}", produces = ["application/json"])
+    suspend fun getPage1Words(@PathVariable("pageNum") pageNum : String): ResponseEntity<WordsDTO> {
         val wordsDTO = WordsDTO(
-            vocabRepository.readAllWords()
+            vocabRepository.readAllWords(Integer.parseInt(pageNum))
                 .map { word ->
                     WordDTO(
                         word.word,
@@ -22,7 +23,8 @@ class VocabController(@Autowired val vocabRepository: VocabRepository) {
                                 meaning.synonyms,
                                 meaning.examples
                             )
-                        })
+                        },
+                        word.paginationToken)
                 })
         return ResponseEntity.ok(wordsDTO);
     }
