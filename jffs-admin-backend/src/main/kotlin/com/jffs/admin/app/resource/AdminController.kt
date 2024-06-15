a58530a19b9b59d8b3d03a5a09f7b68451900d9a
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-06-15 11:20:29 +0100

    14: Paginated API implementation with manual testing

--- jffs-admin-backend/src/main/kotlin/com/jffs/admin/app/resource/AdminController.kt
+++ jffs-admin-backend/src/main/kotlin/com/jffs/admin/app/resource/AdminController.kt
@@ -11,21 +11,26 @@ import org.springframework.web.bind.annotation.PathVariable
 @Controller
 class AdminController(@Autowired val vocabRepository: VocabRepository) {
     @GetMapping("/v1/words/{pageNum}", produces = ["application/json"])
-    suspend fun getPage1Words(@PathVariable("pageNum") pageNum : String): ResponseEntity<WordsDTO> {
-        val wordsDTO = WordsDTO(
-            vocabRepository.readAllWords(Integer.parseInt(pageNum))
-                .map { word ->
-                    WordDTO(
-                        word.word,
-                        word.meanings.map { meaning: Meaning ->
-                            MeaningDTO(
-                                meaning.definition,
-                                meaning.synonyms,
-                                meaning.examples
-                            )
-                        },
-                        word.paginationToken)
-                })
-        return ResponseEntity.ok(wordsDTO);
+    suspend fun getPaginatedWords(@PathVariable("pageNum") pageNum: String): ResponseEntity<PageinatedWordsDTO> {
+        val paginatedWords = vocabRepository.readAllWords(Integer.parseInt(pageNum))
+        val words = paginatedWords.words
+            .map { word ->
+                WordDTO(
+                    word.word,
+                    word.meanings.map { meaning: Meaning ->
+                        MeaningDTO(
+                            meaning.definition,
+                            meaning.synonyms,
+                            meaning.examples
+                        )
+                    })
+            }
+        return ResponseEntity.ok(
+            PageinatedWordsDTO(words,
+                paginatedWords.totalPages,
+                paginatedWords.currentPage,
+                paginatedWords.nextPage,
+                paginatedWords.previousPage,
+                ));
     }
 }
