cb006e8fcbc1adabf9976f6c9103e20cd73c7989
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-06-16 21:44:29 +0100

    Next and Previous buttons

--- jffs-admin-ui/src/organisms/VocabList.js
+++ jffs-admin-ui/src/organisms/VocabList.js
@@ -4,29 +4,62 @@ import { getWords } from "../api/backend_api";
 export default function VocabList() {
   const [allWords, setAllWords] = useState();
   const [nextPage, setNextPage] = useState(1);
-  
-  useEffect(() => {
+  const [previousPage, setPreviousPage] = useState(-1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handleNext = () => {
+    getData(nextPage);
+  }
+
+  const handlePrevious = () => {
+    getData(previousPage);
+  }
+
+  function getData(nextPage) {
     getWords(nextPage)
       .then((response) => {
         var data = response.data["words"];
         const rows = [];
         for (let i = 0; i < data.length; i++) {
           rows.push(
-            <tr className="table-primary">
+            <tr key={i} className="table-primary">
               <td>{data[i]["word"]}</td>
               <td>{data[i]["meanings"][0]["definition"]}</td>
             </tr>
           );
         }
         setAllWords(rows);
+        setTotalPages(response.data["totalPages"]);
+        setPreviousPage(response.data["previousPage"]);
+        setNextPage(response.data["nextPage"]);
+        setCurrentPage(response.data["currentPage"]);
       })
       .catch((error) => {
         console.log(error);
       });
+  }
+ 
+  useEffect(() => {
+    getData(nextPage);
   }, []);
 
   return (
     <>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination justify-content-center">
+          <li id="previous" className={previousPage === -1 ? "page-item disabled" : "page-item"}>
+            <a className="page-link" href="#" onClick={handlePrevious}>
+              Previous
+            </a>
+          </li>
+          <li id="next" className={nextPage === -1 ? "page-item disabled" : "page-item"}>
+            <a className="page-link" onClick={handleNext}>
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
       <div>
         <table className="table table-striped">
           <thead>
