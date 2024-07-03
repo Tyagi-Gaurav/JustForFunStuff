3f509d769439b2af7c5c3a72d6fae1e64a9fdb33
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-03 16:41:10 +0100

    More end to end tests

--- jffs-end-to-end-tests/src/test/java/com/jffs/e2e/tests/core/WithPlaywrightWrapperAssertions.java
+++ jffs-end-to-end-tests/src/test/java/com/jffs/e2e/tests/core/WithPlaywrightWrapperAssertions.java
@@ -10,6 +10,14 @@ public interface WithPlaywrightWrapperAssertions {
         return Locator::isVisible;
     }
 
+    default Function<Locator, Boolean> hasValue(String value) {
+        return locator -> value.equals(locator.innerText());
+    }
+
+    default Function<Locator, Boolean> hasValueLike(String valueRegex) {
+        return locator -> locator.innerText().matches(valueRegex);
+    }
+
     default Function<Locator, Boolean> isDisabled() {
         return Locator::isDisabled;
     }
