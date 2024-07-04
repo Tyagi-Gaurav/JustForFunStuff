7f90fe6447e70bb565f3979277fb190e1612f38d
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-04 22:10:37 +0100

    Echo latest tag

--- release-project.sh
+++ release-project.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Current release tag is $RELEASE_TAG"
+echo "Current release tag is ${RELEASE_TAG}"
 
 cd jffs-ui || exit
 #docker build -t chonku/jffs-ui:LATEST -t chonku/jffs-ui:"${CURRENT_TAG}" . || cd ..
