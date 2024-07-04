7f12f95ed138f984cf7c9f887b75af2cfb8e8b4d
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-04 16:58:39 +0100

    Export and reuse latest tag in other actions

--- release-project.sh
+++ release-project.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-CURRENT_TAG=$(git describe --abbrev=0 --tags)
-
-echo "Current release tag is $CURRENT_TAG"
+echo "Current release tag is $RELEASE_TAG"
 
 cd jffs-ui || exit
-docker build -t chonku/jffs-ui:LATEST -t chonku/jffs-ui:"${CURRENT_TAG}" . || cd ..
+#docker build -t chonku/jffs-ui:LATEST -t chonku/jffs-ui:"${CURRENT_TAG}" . || cd ..
 
-docker image push chonku/jffs-ui:"${CURRENT_TAG}"
+#docker image push chonku/jffs-ui:"${CURRENT_TAG}"
 
 #mvn clean package
