b0690bd81039dc7e4c7283a5a8cbb803160cf966
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-04 15:27:27 +0100

    List all releases

--- release-project.sh
+++ release-project.sh
@@ -6,13 +6,19 @@ echo "Next release tag is $NEW_TAG"
 echo "OWNER is ${OWNER}"
 echo "REPO is ${REPO}"
 
-BODY="{\"tag_name\":\"v${NEW_TAG}\",\"target_commitish\":\"master\",\"name\":\"v${NEW_TAG}\",\"draft\":false,\"prerelease\":false,\"generate_release_notes\":true}"
 curl -L \
-  -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GH_ALL_ACCESS_TOKEN}"\
+  -H "Authorization: Bearer ${GH_ALL_ACCESS_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/"${OWNER}"/"${REPO}"/releases \
-  -d "${BODY}"
+  https://api.github.com/repos/"${OWNER}"/"${REPO}"/releases
+
+#BODY="{\"tag_name\":\"v${NEW_TAG}\",\"target_commitish\":\"master\",\"name\":\"v${NEW_TAG}\",\"draft\":false,\"prerelease\":false,\"generate_release_notes\":true}"
+#curl -L \
+#  -X POST \
+#  -H "Accept: application/vnd.github+json" \
+#  -H "Authorization: Bearer ${GH_ALL_ACCESS_TOKEN}"\
+#  -H "X-GitHub-Api-Version: 2022-11-28" \
+#  https://api.github.com/repos/"${OWNER}"/"${REPO}"/releases \
+#  -d "${BODY}"
 
 #gh release create "$NEW_TAG" --generate-notes
