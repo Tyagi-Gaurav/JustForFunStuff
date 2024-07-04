db858bd63e0f8ab7e0b69457fc67ed1586341f0e
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-04 16:45:18 +0100

    Github login to docker hub inside release.yml

--- release-project.sh
+++ release-project.sh
@@ -7,8 +7,6 @@ echo "Current release tag is $CURRENT_TAG"
 cd jffs-ui || exit
 docker build -t chonku/jffs-ui:LATEST -t chonku/jffs-ui:"${CURRENT_TAG}" . || cd ..
 
-docker login registry-1.docker.io --username "${DOCKER_HUB_USERNAME}" --password "${DOCKER_HUB_PASSWORD}"
-
 docker image push chonku/jffs-ui:"${CURRENT_TAG}"
 
 #mvn clean package
