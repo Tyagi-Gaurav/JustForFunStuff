f2e0510f8a92c86654ea7ada398d3f3004760ab2
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-04 16:14:17 +0100

    Create docker images inside release script

--- release-project.sh
+++ release-project.sh
@@ -1,24 +1,11 @@
 #!/usr/bin/env bash
 
-NEW_TAG=$(git describe --tags --abbrev=0 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+CURRENT_TAG=$(git describe --abbrev=0 --tags)
 
-echo "Next release tag is $NEW_TAG"
-echo "OWNER is ${OWNER}"
-echo "REPO is ${REPO}"
+echo "Current release tag is $CURRENT_TAG"
 
-curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GH_ALL_ACCESS_TOKEN}" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/"${OWNER}"/"${REPO}"/releases
+cd jffs-ui || exit
+docker build -t chonku/jffs-ui:LATEST -t chonku/jffs-ui:"${CURRENT_TAG}" . || cd ..
+docker image push chonku/jffs-ui:"${CURRENT_TAG}"
 
-#BODY="{\"tag_name\":\"v${NEW_TAG}\",\"target_commitish\":\"master\",\"name\":\"v${NEW_TAG}\",\"draft\":false,\"prerelease\":false,\"generate_release_notes\":true}"
-#curl -L \
-#  -X POST \
-#  -H "Accept: application/vnd.github+json" \
-#  -H "Authorization: Bearer ${GH_ALL_ACCESS_TOKEN}"\
-#  -H "X-GitHub-Api-Version: 2022-11-28" \
-#  https://api.github.com/repos/"${OWNER}"/"${REPO}"/releases \
-#  -d "${BODY}"
-
-#gh release create "$NEW_TAG" --generate-notes
+#mvn clean package
