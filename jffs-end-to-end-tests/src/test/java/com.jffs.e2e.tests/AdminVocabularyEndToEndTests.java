9adf2e9b6b66a936243af545877f9d8ea38f45e3
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-08 00:00:48 +0100

    Adding test for Deleting a word via Admin App

--- jffs-end-to-end-tests/src/test/java/com.jffs.e2e.tests/AdminVocabularyEndToEndTests.java
+++ jffs-end-to-end-tests/src/test/java/com.jffs.e2e.tests/AdminVocabularyEndToEndTests.java
@@ -3,11 +3,17 @@ package com.jffs.e2e.tests;
 import com.jffs.e2e.tests.core.AbstractEndToEndTests;
 import com.jffs.e2e.tests.core.WithAdminApp;
 import com.jffs.e2e.tests.core.WithSyntacticSugar;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static com.jffs.e2e.tests.TestWordBuilder.aWord;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
@@ -113,6 +119,41 @@ class AdminVocabularyEndToEndTests extends AbstractEndToEndTests implements With
             when(aButton(withName(PREVIOUS_BUTTON)), isClicked());
             thenEventually(aLabel(byText("Page 1 of 3")), isVisible());
             thenEventually(aButton(withName(PREVIOUS_BUTTON)), isDisabled());
+        }
+    }
+
+    @Nested
+    class DeleteItem {
+        @Test
+        void deleteWord() throws Exception {
+            page.navigate(adminAppUrl());
+            givenSomeWordsExist();
+
+            givenExists(aWord("WordToBeDeleted")
+                    .withDefinition("A Definition that should be deleted")
+                    .withSynonyms(List.of("Synonym1","Synonym2"))
+                    .withExamples(List.of("Example1", "Example2")));
+
+            givenListItemIsClicked();
+            thenEventually(aLabel(byText("Page 1 of 3")), isVisible());
+            given(searchByWordFor(), isFilledWith("WordToBeDeleted"));
+            given(aButton(withName("GO")), isClicked());
+
+            thenEventually(aCell(withText("WordToBeDeleted")), isVisible());
+            given(aCell(withText("WordToBeDeleted")), isClicked());
+            and(aButton(withName("DELETE")), isVisible());
+            when(aButton(withName("DELETE")), isClicked());
+
+            Assertions.assertThat(doesWordExist("WordToBeDeleted")).isFalse();
+        }
+
+        private Consumer<Locator> isFilledWith(String text) {
+            return locator -> locator.fill(text);
+        }
+
+        private Function<Page, Locator> searchByWordFor() {
+            Locator word = page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Word"));
+            return page -> word;
         }
     }
 
