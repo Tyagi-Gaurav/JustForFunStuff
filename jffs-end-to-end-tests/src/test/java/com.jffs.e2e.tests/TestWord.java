b42b7bea63f864931411269be80e99730e999cb6
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-08 21:51:59 +0100

    Add test for adding a new word using admin app

--- jffs-end-to-end-tests/src/test/java/com.jffs.e2e.tests/TestWord.java
+++ jffs-end-to-end-tests/src/test/java/com.jffs.e2e.tests/TestWord.java
@@ -3,7 +3,7 @@ package com.jffs.e2e.tests;
 import java.util.List;
 
 public record TestWord(String word, List<TestMeaning> meanings) {
+    public record TestMeaning(String definition, List<String> synonyms, List<String> examples) {
+    }
 }
 
-record TestMeaning(String definition, List<String> synonyms, List<String> examples) {
-}
