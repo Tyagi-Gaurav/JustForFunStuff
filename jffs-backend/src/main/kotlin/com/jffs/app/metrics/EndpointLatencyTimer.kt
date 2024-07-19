a42e256053ae541d9d712903a2fbfaef5679c119
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-19 19:25:14 +0100

    Collecting metrics

--- jffs-backend/src/main/kotlin/com/jffs/app/metrics/EndpointLatencyTimer.kt
+++ jffs-backend/src/main/kotlin/com/jffs/app/metrics/EndpointLatencyTimer.kt
@@ -2,6 +2,7 @@ package com.jffs.app.metrics
 
 import com.jffs.app.metrics.MetricConstants.METHOD_TAG
 import com.jffs.app.metrics.MetricConstants.PATH_TAG
+import com.jffs.app.metrics.MetricConstants.REQUEST_LATENCY
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Timer
 import java.util.concurrent.TimeUnit
@@ -9,7 +10,7 @@ import java.util.concurrent.TimeUnit
 class EndpointLatencyTimer(private val meterRegistry: MeterRegistry) {
 
     fun observe(startTime: Long, methodTag: String, pathTag: String) {
-        val timer = Timer.builder("request.latency")
+        val timer = Timer.builder(REQUEST_LATENCY)
             .tags(METHOD_TAG, methodTag, PATH_TAG, pathTag)
             .publishPercentiles(0.5, 0.95, 0.99)
             .register(meterRegistry)
--- jffs-end-to-end-tests/src/test/java/com.jffs.e2e.tests/VocabularyEndToEndTest.java
+++ jffs-end-to-end-tests/src/test/java/com.jffs.e2e.tests/VocabularyEndToEndTest.java
@@ -4,7 +4,10 @@ import com.jffs.e2e.tests.core.AbstractEndToEndTests;
 import com.jffs.e2e.tests.core.WithJffsApp;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static java.lang.Thread.sleep;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class VocabularyEndToEndTest extends AbstractEndToEndTests implements WithJffsApp {
 
@@ -35,5 +38,44 @@ class VocabularyEndToEndTest extends AbstractEndToEndTests implements WithJffsAp
 
         thenEventually(aButton(withText("Begin")), not(isVisible()));
         thenEventually(aButton(withText("Next")), isVisible());
+    }
+
+    @Test
+    void metricsGeneratedForApi() throws Exception {
+        givenSomeWordsExist();
+        page.navigate(appUrlWithPath("games/vocabtesting"));
+
+        thenEventually(aLabel(byText("Test your Vocabulary")), isVisible());
+        thenEventually(aLabel(byText("Can you think of the meaning before the timer runs out?")), isVisible());
+
+        thenEventually(aButton(withText("Begin")), isVisible());
+        when(aButton(withText("Begin")), isClicked());
+
+        thenEventually(anElement(byTestId("countdown")), isVisible());
+        and(anElement(byTestId("word-text")), isVisible());
+        and(anElement(byTestId("word-text")), hasValueLike("Word.*"));
+
+        checkMetricExists("request_latency_seconds", "method", "GET", "path", "/api/v1/words", "quantile", "0.5");
+        checkMetricExists("response_status_total", "status", "200");
+        checkMetricExists("request_count_total", "method", "GET", "path", "/api/v1/words");
+    }
+
+    private void checkMetricExists(String metricName, String... tags) throws Exception {
+        final var tagBuilder = new StringBuilder();
+        for (int i = 0; i < tags.length; i+=2) {
+            tagBuilder.append(tags[i]).append("=").append("\"").append(tags[i + 1]).append("\"");
+            if (!(i+2 >= tags.length)) {
+                tagBuilder.append(",");
+            }
+        }
+
+        final var metricBuilder = new StringBuilder();
+        metricBuilder.append(metricName);
+        if (!tagBuilder.isEmpty()) {
+            metricBuilder.append("{").append(tagBuilder).append("}");
+        }
+
+        Map<String, Double> afterMetrics = captureMetrics();
+        assertThat(afterMetrics).containsKey(metricBuilder.toString());
     }
 }
