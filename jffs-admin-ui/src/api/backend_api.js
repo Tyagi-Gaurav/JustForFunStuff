e1944d1601fec455c1ff4a65a0364d1ded8a0358
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-07-20 13:55:46 +0100

    Fix broke build

--- jffs-admin-ui/src/api/backend_api.js
+++ jffs-admin-ui/src/api/backend_api.js
@@ -29,7 +29,7 @@ export const updateWord = (oldWord, word) => {
 };
 
 export const addWord = (word) => {
-  console.log("Adding word: " + word);
+  //console.log("Adding word: " + word);
   return axios.post("/admin/v1/words", word, {
     headers: {
       "Content-Type": "application/vnd+add.word.v1+json",
