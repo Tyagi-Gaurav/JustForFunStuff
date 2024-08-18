c206aeda47ea464f0e34b817c2e1ff148e1059aa
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2024-08-18 22:08:22 +0100

    Find all words with page number argument

--- jffs-admin-backend/src/main/kotlin/com/jffs/admin/app/resource/AdminGraphQLController.kt
+++ jffs-admin-backend/src/main/kotlin/com/jffs/admin/app/resource/AdminGraphQLController.kt
@@ -1,21 +1,40 @@
 package com.jffs.admin.app.resource
 
+import com.jffs.admin.app.db.AdminRepository
+import com.jffs.admin.app.domain.Meaning
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
 @Controller
-class AdminGraphQLController {
+class AdminGraphQLController(@Autowired val adminRepository: AdminRepository) {
     private val LOG: Logger = LogManager.getLogger("APP")
 
     @QueryMapping
-    fun allWords(): PaginatedWordsDTO {
-        return PaginatedWordsDTO(
-            listOf(
+    suspend fun allWords(@Argument pageNum: Int): PaginatedWordsDTO {
+        val paginatedWords = adminRepository.readAllWords(pageNum)
+        val words = paginatedWords.words
+            .map { word ->
                 WordDTO(
-                    "aWord",
-                    listOf(MeaningDTO("aDefinition", listOf("Synonyms"), listOf("Examples")))
-                )), 1, 1, 1, -1, -1)
+                    word.word,
+                    word.meanings.map { meaning: Meaning ->
+                        MeaningDTO(
+                            meaning.definition,
+                            meaning.synonyms,
+                            meaning.examples
+                        )
+                    })
+            }.toList()
+        return PaginatedWordsDTO(
+            words,
+            paginatedWords.totalWords,
+            paginatedWords.totalPages,
+            paginatedWords.currentPage,
+            paginatedWords.nextPage,
+            paginatedWords.previousPage,
+        )
     }
 }
--- jffs-admin-backend/src/main/kotlin/com/jffs/admin/app/resource/DemoGraphQL.kt
+++
@@ -1,5 +0,0 @@
-package com.jffs.admin.app.resource
-
-class DemoGraphQL {
-
-}
